02cbd0fe4beac204c01b09fc502717decb95bfff
sorland2 <sorland2@05618fc6-2026-4b5c-8859-2673b8f0ccee>
2014-06-25 14:46:58 +0000

    update of argparser

--- man/python/arg_parser/parser.py
+++ man/python/arg_parser/parser.py
@@ -2,14 +2,20 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='some descriptions', epilog='last line')
+parser = argparse.ArgumentParser(prog='myprogram', description='some descriptions', epilog='print this akin last line')
 
-parser.add_argument('-s', '--long')
+
+parser.add_argument('-f', '--foo', help='help for foo option here')
+parser.add_argument('-b', '--bar', help='bar help', nargs=1)
+
+#parser.add_argument('-c', '--cc', help='help for option here', nargs='+')
+
 #parser.add_argument('-c', '--const', action='store_const', const=42)
-#parser.add_argument('arg1')
 
 args = parser.parse_args()
 
-print args.s
+print args
+
+print args.foo
 
 
