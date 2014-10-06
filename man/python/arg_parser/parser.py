a4ecf32e55e7a5eab4ef99cc2193196f4dba1a6e
sorland2 <sorland2@05618fc6-2026-4b5c-8859-2673b8f0ccee>
2014-10-06 19:16:45 +0000

    add mutually exsclusive example in argparse

--- man/python/arg_parser/parser.py
+++ man/python/arg_parser/parser.py
@@ -10,8 +10,18 @@ parser.add_argument('-b', '--bar', help='bar help', nargs=1)
 parser.add_argument('optional positional args', default=[], nargs='*')
 #parser.add_argument('required positional args', default=[], nargs='+')
 
-parser.add_argument('--foo-one', type = int, help=' ')
-parser.add_argument('--foo-two', type = int, help=' ')
+
+# mutually exclusive options
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-x', action='store_true')
+group.add_argument('-y', action='store_true')
+
+# mutually exclusive options with default value
+group2 = parser.add_mutually_exclusive_group()
+parser.set_defaults(mutex='on')
+group2.add_argument('--on', action='store_const', dest='mutex', const='on')
+group2.add_argument('--off', action='store_const', dest='mutex', const='off')
+
 
 args = parser.parse_args()
 
